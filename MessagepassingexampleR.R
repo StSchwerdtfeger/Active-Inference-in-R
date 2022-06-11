13defc53fea093b42b5c8aaae8f7ee3093025412
Steffen Schwerdtfeger <88867126+StSchwerdtfeger@users.noreply.github.com>
2022-06-11 14:47:26 +0200

    Add files via upload

--- MessagepassingexampleR.R
+++ MessagepassingexampleR.R
@@ -4,12 +4,14 @@
 #--------------------------------#
 ##################################
 
-# CONVERSION of MATlab code Message_passing_example.m, including commentaries.
-
 # Supplementary Code for: A Tutorial on Active Inference 
 # Modelling and its Application to Empirical Data by Ryan Smith 
 # and Christopher J. Whyte. We also acknowledge Samuel Taylor for contributing 
-# to this example code
+# to this example code.
+
+# CONVERSION of Matlab code Message_passing_example.m. The commentary
+# is included as well and extendet mostly by those that refer to R code
+# exclusively. Conversion by Steffen Schwerdtfeger https://github.com/StSchwerdtfeger
 
 # This script provides two examples of (marginal) message passing, based on
 # the steps described in the main text. Each of the two examples (sections)
@@ -65,8 +67,6 @@ library(pracma) # for gradient(), numerical gradiant Matlab-Style
 # Note that some steps (7 and 8) appear out of order when they involve loops that
 # repeat earlier steps
 
-library(dplyr)
-
 # Softmax function
 softmax <- function(par){             # THANKS TO: https://rpubs.com/FJRubio/softmax
   n.par <- length(par)                # equivalent: exp(result)/sum(exp(result))
@@ -91,7 +91,7 @@ A = matrix(c(.9, .1,
            nrow = 2,
            ncol = 2,
            byrow= TRUE) 
- 
+
 # transitions
 B = matrix(c(1, 0,
              0, 1),
@@ -105,66 +105,69 @@ Timepoints = 2
 # number of iterations of message passing
 NumIterations = 16
 
-# at exp(-16) to log inputs as log(0) is not defined
+# Add exp(-16) to log inputs as log(0) is not defined.
+# A function is usually defined for this purpose. 
+# However, we will just add the term z to every log-input.
 z = exp(-16)
 
-# Initialize posterior STEP 1
-qs = c(.5,.5)
-
-# List to store results
-qsLIST <- vector("list", 16*2)
-dim(qsLIST) <- matrix(c(16,2))
-
-
-for (Ni in 1:NumIterations){
-  # fix observations (STEP 2) [using a list]
-  o<- vector("list", 1*2)
-  dim(o) = matrix(c(1,2))
-  # Observations
-  o[[1]] = matrix(c(1, 0))
-  o[[2]] = matrix(c(1, 0))
-
-    # For each edge (hidden state) - STEP 7 
-    for (tau in 1:Timepoints){
-      # Choose an edge
-      q = log(qs[tau]+z)   
-      
-      # compute MEssage sent by D and B (STEP 4) using the posterior
-      # computed in 6B
-      if (tau == 1){  # first time point
-        lnD = log(D+z)  # Message 1
-        lnBs= log(t(B)%*%qs[]+z)
-      }
-      else if (tau == Timepoints){
-        lnBs= log(B%*%qs[]+z)
-      }
+for (Ni in 1:NumIterations){ # START of loop
+  if (Ni == 1){ # set up qs and corresponding list
+    # Initialize posterior STEP 1
+    qs = c(.5,.5)
+    
+    # List to store results
+    qsLIST <- vector("list", 16*2)
+    dim(qsLIST) <- matrix(c(16,2))
+    
+    # fix observations (STEP 2) [using a list]
+    o<- vector("list", 1*2)
+    dim(o) = matrix(c(1,2))
+    # Observations
+    o[[1]] = matrix(c(1, 0)) # 
+    o[[2]] = matrix(c(1, 0)) # same observation for each tau!
+    
+  }
+  
+  # For each edge (hidden state) - STEP 7 
+  for (tau in 1:Timepoints){
+    # Choose an edge
+    q = log(qs[tau]+z)   
+    
+    # compute Message sent by D and B (STEP 4) using the posterior
+    # computed in 6B
+    if (tau == 1){  # first time point
+      lnD = log(D+z)  # Message 1
+      lnBs= log(t(B)%*%qs[]+z)
+    }
+    else if (tau == Timepoints){
+      lnBs= log(B%*%qs[]+z)
+    }
       lnAo = log((t(A)%*%as.vector(o[[tau]]))+z)  # Likelihood (as.vector(o...)) 
       
-      # STEPS 5-6 (Pass messages and update the posterior)
-      # Since all terms are in log space, this is addition instead of
-      # multiplication. This corresponds to equation 16 in the main
-      # text (within the softmax)
-      if (tau == 1){
-        q = .5*lnD+.5*lnBs + as.vector(lnAo)
-      }
-      else if (tau == Timepoints){
-        q = .5*(lnBs) + as.vector(lnAo)
-      }
-      # normalize using a softmax function to find posterior (STEP 6A)
-      qs = softmax (q)
-      qsLIST[[Ni,tau]]<- qs # store value for each iteration
-    } 
+    # STEPS 5-6 (Pass messages and update the posterior)
+    # Since all terms are in log space, this is addition instead of
+    # multiplication. This corresponds to equation 16 in the main
+    # text (within the softmax)
+    if (tau == 1){
+      q = .5*lnD+.5*lnBs + as.vector(lnAo)
+    }
+    else if (tau == Timepoints){
+      q = .5*(lnBs) + as.vector(lnAo)
+    }
+    # normalize using a softmax function to find posterior (STEP 6A)
+    qs = softmax (q)
+    qsLIST[[Ni,tau]]<- qs # store value for each iteration
+  } 
   #  Repeat for remaining edges (STEP 7)
-print(qs) # Repeat until convergence/for fixed number of iterations (STEP 8)
-#print(qsResults)
-}
+  # Repeat until convergence/for fixed number of iterations (STEP 8)
+  print(qs) 
+} # END of loop!
 
 # Final posterior beliefs over states
 qs
 
 # qsLIST of each iteration, for each tau. 
 qsLIST
-
 
 ###################################
 # Plotting firing rates (traces): #
@@ -175,13 +178,15 @@ addD = vector("list", 1*2)
 dim(addD) = matrix(c(1,2))
 addD[[1,1]] <- c(.5,.5)
 addD[[1,2]] <- c(.5,.5)
-addD
+addD 
+
 # Use rbind to place D up front which results in a new list of dim 17*2
+# (((The Matlab script uses: qs_plot = [D' D';qs(:,:,1) qs(:,:,2)])))
 qsLISTplot = rbind(addD, qsLIST)
 
-# Transform upper list into a dataframe. Not that the list neeeds
-# to be transposed for this via t(a). This results in a dataframe
-# with a dim of 17*4
+# Transform upper list into a dataframe to use $. Note that the elememts of
+# the list have to be unlisted and to be transposed for this fist via t(a). 
+# This results in a dataframe with a dim of 17*4. 
 qsPLOT <- data.frame(matrix(unlist(t(qsLISTplot)), nrow=17, ncol=4,byrow=TRUE),stringsAsFactors = TRUE)
 qsPLOT
 
@@ -192,6 +197,13 @@ plot(qsPLOT$X1, type = "l", col= "green",
 lines(qsPLOT$X2, col="blue")     # add the other columns of qsPLOT
 lines(qsPLOT$X3, col="red")
 lines(qsPLOT$X4, col="yellow")
+
+# GREEN + BLUE: tau = 1
+# RED + YELLOW: tau = 2
+
+# see p. 29, Fig. 8 "Firing rates (upper-right) correspond to the 
+# magnitude of posteriors over each state[...]".
+# RECALL that the observation o was the same over all t.
 
 
 #####################################################
@@ -246,69 +258,79 @@ dim(xn) <- matrix(c(16,2,2))
 
 # Message passing:
 
-for (t in 1:Timesteps){
-  
-      for (Ni in 1:NumIterations){   # STEP 8 loop of VMP
-          
-          for (tau in 1:Timesteps){ # STEP 7 loop of VMP
-            # fix observations sequentially (STEP 2) [using a list of lists]
-            # Each tau for Timestep1
-            o1<- vector("list", 1*2)
-            dim(o1) = matrix(c(1, 2))
-            o1[[1]] = matrix(c(1, 0))
-            o1[[2]] = matrix(c(0, 0))
-            
-            # Each tau for Timestep1
-            o2<- vector("list", 1*2)
-            dim(o2) = matrix(c(1,2))
-            o2[[1]] = matrix(c(1, 0))       
-            o2[[2]] = matrix(c(1, 0))               
-            
-            # list of lists
-            o <- vector("list", 2*2)
-            dim(o) = matrix(c(2,2))
-            o[1,] <- o1 # assign to list
-            o[2,] <- o2 #      -''-
-
-          # initialise depolarization variable: v = ln(s)
-          # choose an edge (Step 3 of VMP)
-          v = log(qs2[])
-            
-            # get correct D and B for each time point (Steps 4-5 of VMP)
-            # using using the posterior computed in Step 6B  
-            if (tau == 1){  # first time point
-            lnD = log(D+z)  # pas(Message 1)
-            
-            lnBs= log(t(B)%*%qs2[]+z) # future (Message 2)
-           }
-            else if (tau == Timesteps){ # last time point
-            lnBs= log(B%*%qs2[]+z)      # no contribution from future (only
-                                        # Message 1)
-           } 
-             # Likelihood Message 3
-            lnAo = log((t(A)%*%as.vector(o[[t,tau]]))+z)  # LIKELIHOOD (as.vector(o...)
+for (t in 1:Timesteps){   # START of loop
+  if(t == 1){
+    # Initialize posterior STEP 1
+    qs2 = c(.5,.5)
     
-             # calculate state prediction error: equation 24
-            if (tau == 1){
-            epsilon = .5*lnD+.5*lnBs + as.vector(lnAo) - v
-           }
-            else if (tau == Timesteps){
-            epsilon = .5*(lnBs) + as.vector(lnAo) - v
-      }
+    # List to store results
+    xn <- vector("list", 16*2*2)
+    dim(xn) <- matrix(c(16,2,2))
+    
+    # fix observations sequentially (STEP 2) [using a list of lists]
+    # Each tau for Timestep 1
+    o1<- vector("list", 1*Timesteps)
+    dim(o1) = matrix(c(1, 2))
+    o1[[1]] = matrix(c(1, 0)) # o at t = 1, tau = 1
+    o1[[2]] = matrix(c(0, 0)) # o at t = 1, tau = 2
+    
+    # Each tau for Timestep 2
+    o2<- vector("list", 1*Timesteps)
+    dim(o2) = matrix(c(1,2))
+    o2[[1]] = matrix(c(1, 0)) # o at t = 1, tau = 1       
+    o2[[2]] = matrix(c(1, 0)) # o at t = 2, tau = 2              
+    
+    # list of lists for all o
+    o <- vector("list", Timesteps*Timesteps)
+    dim(o) = matrix(c(2,2))
+    o[1,] <- o1 # assign to list
+    o[2,] <- o2 #      -''-
+  } # END if t == 1 for setup lists etc.
+  
+  for (Ni in 1:NumIterations){             # STEP 8 loop of VMP
+      
+    for (tau in 1:Timesteps){              # STEP 7 loop of VMP
+      # initialise depolarization variable: v = ln(s)
+      # choose an edge (Step 3 of VMP)
+      v = log(qs2) # different to Matlab qs2 is always the current t 
+                   # and not obtained from a list such as e.g. qs2[[t]]
             
-            # (Step 6 of VMP)
-            # update depolarization variable: equation 25
-            v = v + epsilon[]
-
-            # normalize using a softmax function to find posterior (STEP 6A of VMP)
-            # equation 26 (Step 6A of VMP)
-            qs2 = softmax (v)
-            xn[[Ni,tau,t]]<- qs2 # store qs for firing rate plots
+      # get correct D and B for each time point (Steps 4-5 of VMP)
+      # using using the posterior computed in Step 6B  
+      if (tau == 1){                  # first time point
+        lnD = log(D+z)                # past (Message 1)
+        lnBs= log(t(B)%*%qs2+z)       # future (Message 2)
       } 
-           #  Repeat for remaining edges (STEP 7 of VMP)
-    }  # Repeat until convergence/for number of iterations (Step 8 of VMP)
+      else if (tau == Timesteps){     # last time point
+        lnBs= log(B%*%qs2+z)          # no contribution from future (only
+                                      # Message 1)
+      } 
+      
+      lnAo = log((t(A)%*%as.vector(o[[t,tau]]))+z)  # Likelihood/present 
+                                                    #    => Message 3
+    
+      # Calculate state prediction error: equation 24
+      if (tau == 1){
+        epsilon = .5*lnD + .5*lnBs + as.vector(lnAo) - v
+      } 
+      else if (tau == Timesteps){
+        epsilon = .5*(lnBs) + as.vector(lnAo) - v
+      } 
+            
+      # (Step 6 of VMP)
+      # update depolarization variable: equation 25
+      v = v + epsilon[]
+
+      # normalize using a softmax function to find posterior (STEP 6A of VMP)
+      # equation 26 (Step 6A of VMP)
+      qs2 = softmax (v)
+      xn[[Ni,tau,t]]<- qs2 # store qs for firing rate plots
+      } # END LOOP OVER tau
+      #  Repeat for remaining edges (STEP 7 of VMP)
+    } # END LOOP OVER Ni 
+    # Repeat until convergence/for number of iterations (Step 8 of VMP)
   print(qs2)
-}
+} # END of loop (over t)
 
 # Final posterior beliefs over states
 qs2
@@ -324,19 +346,20 @@ xn
 # ERP - Event Related Potentials #
 ##################################
 
+
 # Getting Data in Form. In the Matlab code this is, e.g., done
 # via implemented functions like spm_cat... # https://github.com/neurodebian/spm12/blob/master/spm_cat.m
-# spm_cat could maybe also be converted into R, making the below much easier...
-
 
 library(pracma) # library for gradient(), calculates numerical gradient 
                 # in Matlab-Style. gradient() for central difference!
+
 
 # Example:
 x= 1:10
 # 1 2 3 4 5 6 7 8 9 10
 gradient(x)
 # 1 1 1 1 1 1 1 1 1 1 
+
 
 # Set up List for ERP plot:
 ERPplot=vector("list", 2*4)
@@ -401,11 +424,11 @@ ERPplusZERO
 
 plot(ERPplusZERO[,1], type = "l", col = "green",
      ylim = c(-.05,.05), # Set dimension y axis 0 to 1
-     xlab = "Message passing iterations", ylab = "Response") # Name labes
+     xlab = "Message passing iterations", ylab = "Response", # Name labels
+             main = "Event related potentials")
 lines(ERPplusZERO[,2], col="blue")     # add the other columns of xnplot
 lines(ERPplusZERO[,3], col="red")
 lines(ERPplusZERO[,4], col="orange")
-
 
 
 
@@ -464,7 +487,7 @@ dim(prior) = matrix(c(1,2))
 priorD = matrix(c(.5,.5), ncol=1, nrow=2, byrow=TRUE)
 prior[[1,1]] = priorD
 prior[[1,2]] = priorD
-prior
+prior 
 
 firepre2=rbind(prior, firepre)
 firepre2
@@ -477,5 +500,6 @@ fireplot <- data.frame(matrix(unlist(t(firepre3)), nrow=4, ncol=33,byrow=FALSE),
 fireplot
 
 # FINALLY: Plot fire rate!
+
 imagesc(1-as.matrix(fireplot), col = grey.colors(256))
 
